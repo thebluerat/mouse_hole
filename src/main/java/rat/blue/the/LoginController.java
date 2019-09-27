89535b3be8beb5d92d6d5b4be494c55433f96bc6
GD7 <GD7@DESKTOP-58VHOFH>
2019-09-27 17:52:44 +0900

    디비 연결 설정, http://ksm.gudi.kr/mouse_hole/ 에 화면이 나온다.

--- src/main/java/rat/blue/the/LoginController.java
+++ src/main/java/rat/blue/the/LoginController.java
@@ -1,5 +1,8 @@
 package rat.blue.the;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,7 +14,21 @@ public class LoginController {
 
 	@RequestMapping("/main")
 	public void login(HttpServletRequest req, HttpServletResponse res) {
-		String url = "https://kauth.kakao.com";
+		String url = "https://kauth.kakao.com/oauth/authorize";
+		url += "?client_id=79bbdf8e5a20d21e5549bb13fb8e44de";
+		try {
+			
+			url	+= URLEncoder.encode("&redirect_uri=http://gdj16.gudi.kr:20006/hi", "UTF-8");
+			url	+= "&response_type=code";
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 	}
 	
+	@RequestMapping("/hi")
+	public void hi() {
+		System.out.println("hi");
+	}
 }
