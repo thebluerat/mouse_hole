4cb9ddcf79396c9cbbeb681388e55fc4798720d7
GD7 <GD7@DESKTOP-58VHOFH>
2019-10-07 18:17:44 +0900

    캔버스 그린 그림 저장해보기

---
+++ src/main/java/rat/blue/the/makeImgFile.java
@@ -0,0 +1,39 @@
+package rat.blue.the;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.apache.commons.codec.binary.Base64;
+
+public class makeImgFile {
+	
+	private void makePngFile(String imgbase64, File saveFilePath, String savename) throws Exception {
+		/**
+		 * imgbase64 (imgbase64data:image/png;base64,iVBORw0KGgoAA 로 시작)
+		 * saveFilePath (저장경로)
+		 * savename (파일이름)
+		 */ 
+		try {
+			// create a buffered image
+			BufferedImage image = null;
+
+			String[] base64Arr = imgbase64.split(","); // image/png;base64, 이 부분 버리기 위한 작업
+			byte[] imageByte = Base64.decodeBase64(base64Arr[1]); // base64 to byte array로 변경
+			
+			ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+			image = ImageIO.read(bis);
+			bis.close();
+
+			// write the image to a file
+			savename = savename;
+			File outputfile = new File(saveFilePath + savename + ".png");
+			ImageIO.write(image, "png", outputfile); // 파일생성
+			
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+}
--- src/main/webapp/WEB-INF/views/draw.jsp
+++ src/main/webapp/WEB-INF/views/draw.jsp
@@ -1,4 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	request.setCharacterEncoding("utf-8");
+	String imgUrl = request.getParameter("imgUrl");
+	
+	System.out.println(imgUrl);
+	
+	
+
+%>
 <!DOCTYPE html>
 <html lang="ko"></html>
 <html>
